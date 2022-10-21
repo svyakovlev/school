8d4ac6c13f004df3ca42c28d1ea4acc4e205188d
Сергей <s.v.yakovlev@yandex.ru>
2022-10-21 23:09:44 +0400

    homework 3.2

--- src/main/java/ru/hogwarts/school/SchoolApplication.java
+++ src/main/java/ru/hogwarts/school/SchoolApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SchoolApplication.class, args);
-
 	}
 
 }
