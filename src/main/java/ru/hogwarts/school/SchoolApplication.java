7c883a2a96709bbea4c2a053631521778d851d92
Сергей <s.v.yakovlev@yandex.ru>
2022-10-22 17:09:26 +0400

    step 2

--- src/main/java/ru/hogwarts/school/SchoolApplication.java
+++ src/main/java/ru/hogwarts/school/SchoolApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchoolApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(SchoolApplication.class, args);
-
-	}
+	public static void main(String[] args) {SpringApplication.run(SchoolApplication.class, args);}
 
 }
